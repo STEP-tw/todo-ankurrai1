7a59559317bf1b18afd9eba896f63eb06dbcc572
Ankur Rai <rai.ankur0007@gmail.com>
2018-02-13 23:18:28 +0530

    home and information pages are ready

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const http = require('http');
-const port = 9000;
+const PORT = 9000;
 
 const app=require('./app.js');
 
-// const server = http.createServer(app);
-app.listen(port, () => console.log('Example app listening on port 9000!'))
-// server.listen(port);
-// server.on('error', (e) => console.error(`Server Error :::: ${e.message}`));
-// console.log(`Listening to Port ${port}`);
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
